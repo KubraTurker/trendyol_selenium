06cdec2d4bfe25e32dc086e19e756218e259a274
Kübra Türker <turkerkbr@gmail.com>
2025-03-21 14:37:56 +0300

    Refactoring

--- cases/auth/sign_in_validation.py
+++ cases/auth/sign_in_validation.py
@@ -1,8 +1,3 @@
-# Kullanici login olabiliyor mu kontrolu yapilir
-# 1. Ana sayfaya gidilir
-# 2. Login Butonuna tiklanir
-# 3. Bilgiler bos birakilir
-# 4. Validasyon hatasi dogrulanir
 import unittest
 import logging
 import time
